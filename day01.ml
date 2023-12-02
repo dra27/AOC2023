a3b0abd95466a04e517a9e1e4c99862605e2b072
David Allsopp <david.allsopp@metastack.com>
2023-12-02 20:16:59 +0000

    Day 1 - second puzzle
    As ever, rewriting the first part in the light of the second

--- day01.ml
+++ day01.ml
@@ -5,16 +5,15 @@ module String = struct
     if i = l || i < 0 then
       raise Not_found
     else
-      let c = s.[i] in
-      if f c then
-        c
-      else
-        findaux f s l next (next i)
+      match f s i with
+      | Some r -> r
+      | None ->
+          findaux f s l next (next i)
 
-  let find s f =
+  let first_left s f =
     findaux f s (String.length s) succ 0
 
-  let rfind s f =
+  let first_right s f =
     let l = String.length s in
     findaux f s l pred (pred l)
 end
@@ -23,21 +22,59 @@ let is_digit = function
 | '0'..'9' -> true
 | _ -> false
 
-let code s =
-  int_of_string (Printf.sprintf "%c%c" (String.find s is_digit)
-                                       (String.rfind s is_digit))
+let is_digit1 s i =
+  let c = s.[i] in
+  if is_digit c then
+    Some (Char.code c - 48)
+  else
+    None
+
+let digits = [
+  "zero"; "one"; "two"; "three"; "four";
+  "five"; "six"; "seven"; "eight"; "nine"
+]
+
+let is_digit2 s _ i =
+  let c = s.[i] in
+  match c with
+  | '0'..'9' -> Some (Char.code c - 48)
+  | _ ->
+      let f w = String.sub s i (String.length w) = w in
+      List.find_index f digits
+
+let code1 s =
+  10 * String.first_left s is_digit1 + String.first_right s is_digit1
+
+let code2 s =
+  let is_digit2 = is_digit2 (s ^ "zzzz") in
+  10 * String.first_left s is_digit2 + String.first_right s is_digit2
 
 let test_input_part1 = ["1abc2"; "pqr3stu8vwx"; "a1b2c3d4e5f"; "treb7uchet"]
+
+let test_input_part2 = [
+  "two1nine";
+  "eightwothree";
+  "abcone2threexyz";
+  "xtwone3four";
+  "4nineeightseven2";
+  "zoneight234";
+  "7pqrstsixteen"
+]
 
 let input =
   In_channel.with_open_text "input-01" In_channel.input_lines
 
-let test_part1 =
-  List.fold_left (fun a l -> a + code l) 0 test_input_part1
+let fold_over f l =
+  List.fold_left (fun a l -> a + f l) 0 l
 
-let solution_part1 =
-  List.fold_left (fun a l -> a + code l) 0 input
+let test_part1 = fold_over code1 test_input_part1
+let solution_part1 = fold_over code1 input
+let test_part2 = fold_over code2 test_input_part2
+let solution_part2 = fold_over code2 input
 
 let () =
-  Printf.printf "Day 1; Puzzle 1; test = %d\n%!" test_part1;
-  Printf.printf "Day 1; Puzzle 1 = %d\n%!" solution_part1
+  Printf.printf "Day 1; Puzzle 1; test = %d\n\
+                 Day 1; Puzzle 1 = %d\n\
+                 Day 1; Puzzle 2; test = %d\n\
+                 Day 1; Puzzle 2 = %d\n" test_part1 solution_part1
+                                         test_part2 solution_part2
