ff81159b9ccfc09b5b3ee735ff321f2d8042be6d
David Allsopp <david.allsopp@metastack.com>
2024-08-12 15:21:29 +0100

    Tidy up the representation a bit

--- day10.ml
+++ day10.ml
@@ -1,43 +1,15 @@
-type field = string array
+module Coord = struct
+  type t = int * int
+  let compare : t -> t -> int = Stdlib.compare
+end
 
-let square : field = Array.of_list (String.split_on_char '\n' (String.trim {|
--L|F7
-7S-7|
-L|7||
--L-J|
-L|-JF
-|}))
+module CoordSet = Set.Make(Coord)
 
-let complex : field = Array.of_list (String.split_on_char '\n' (String.trim {|
-7-F7-
-.FJ|7
-SJLL7
-|F--J
-LJ.LJ
-|}))
-
-let get_start field =
-  let check row s =
-    Option.map (fun column -> row, column) (String.index_opt s 'S') in
-  let (row, column) as start = Option.get (Array.find_mapi check field) in
-  let connections = [
-    (['7'; 'F'; '|'], row - 1, column);
-    (['L'; 'F'; '-'], row, column - 1);
-    (['J'; '7'; '-'], row, column + 1);
-    (['L'; 'J'; '|'], row + 1, column);
-  ] in
-  let test_connection (connections, row, column) =
-    if row >= 0 &&
-       column >= 0 &&
-       row < Array.length field &&
-       column < String.length field.(0) &&
-       List.mem field.(row).[column] connections then
-      Some (row, column)
-    else
-      None in
-  match List.filter_map test_connection connections with
-  | [first; second] -> ((start, first), (start, second))
-  | _ -> assert false
+type field = {
+  field: string array;
+  start: Coord.t;
+  loop: CoordSet.t;
+}
 
 module CharMap = Map.Make(Char)
 
@@ -60,49 +32,13 @@ let connections, loop_pieces, pipe_pieces =
     with Not_found -> String.make 1 c in
   connections, wrap loop_pieces, wrap pipe_pieces
 
-let step field (prev, (row, column)) =
-  let ((delta1_row, delta1_column), (delta2_row, delta2_column)) =
-    CharMap.find field.(row).[column] connections in
-  let next1 = (row + delta1_row, column + delta1_column) in
-  let next2 = (row + delta2_row, column + delta2_column) in
-  if next1 = prev then
-    next2
-  else if next2 = prev then
-    next1
-  else
-    assert false
-
-module CoordSet = Set.Make(struct type t = int * int let compare = compare end)
-
-type solved_field = string array * CoordSet.t
-
-let rec walk field loop n (((_, cur1) as walk1), ((_, cur2) as walk2)) =
-  let n = succ n in
-  if cur1 = cur2 then
-    n, (field, loop : solved_field)
-  else
-    let next1 = step field walk1 in
-    let next2 = step field walk2 in
-    if next1 = cur2 && next2 = cur1 then
-      n, (field, loop : solved_field)
-    else
-      let loop = CoordSet.add next1 (CoordSet.add next2 loop) in
-      walk field loop n ((cur1, next1), (cur2, next2))
-
-let walk field =
-  let ((start, next1), (_, next2)) as start_state = get_start field in
-  let loop = CoordSet.of_list [start; next1; next2] in
-  walk field loop 0 start_state
-
-let test1_part1, loop_square = walk square
-let test2_part1, loop_complex = walk complex
-let field : field =
-  In_channel.with_open_text "input-10" @@ fun ic ->
-    Array.of_list (In_channel.input_lines ic)
-let solution_part1, loop = walk field
-
-let print_field_gen loop fmt field =
+let print_field fmt {field; loop; start = (start_row, start_column)} =
   let print_column row column c =
+    let c =
+      if row = start_row && column = start_column then
+        'S'
+      else
+        c in
     let pieces =
       if CoordSet.mem (row, column) loop then
         loop_pieces
@@ -114,11 +50,129 @@ let print_field_gen loop fmt field =
     Format.pp_print_char fmt '\n' in
   Array.iteri print_row field
 
-let print_solved_field fmt (field, loop : solved_field) =
-  print_field_gen loop fmt field
+type north = N
+type south = S
+type east = E
+type west = W
 
-let print_field fmt (field : field) =
-  print_field_gen CoordSet.empty fmt field
+type _ connection =
+| N : north connection
+| S : south connection
+| E : east connection
+| W : west connection
+| X : _ connection
+
+let parse_field lines =
+  let rec scan prev row = function
+  | line::lines ->
+      conclude prev line lines row (String.index_opt line 'S')
+  | [] ->
+      assert false
+  and conclude prev line lines row = function
+  | None ->
+      scan line (succ row) lines
+  | Some column ->
+      let next = match lines with
+      | line::_ -> line
+      | [] -> "" in
+      let get_if cond s column c1 c2 c3 connection =
+        if cond && (s.[column] = c1 || s.[column] = c2 || s.[column] = c3) then
+          connection
+        else
+          X in
+      let north =
+        get_if (row > 0) prev column '|' 'F' '7' N in
+      let south =
+        get_if (lines <> []) next column '|' 'J' 'L' S in
+      let east =
+        get_if (column < String.length line) line (column + 1) '-' 'J' '7' E in
+      let west =
+        get_if (column > 0) line (column - 1) '-' 'L' 'F' W in
+      let start_pipe =
+        match north, south, east, west with
+        | N, S, X, X -> '|'
+        | N, X, E, X -> 'L'
+        | N, X, X, W -> 'J'
+        | X, S, E, X -> 'F'
+        | X, S, X, W -> '7'
+        | X, X, E, W -> '-'
+        | _, _, _, _ -> assert false in
+      start_pipe, (row, column), row + 1 + List.length lines in
+  let start_pipe, (start_row, start_column as start), length =
+    scan "" 0 lines in
+  let field = Array.make length "" in
+  let init index line =
+    let f column c = if column = start_column then start_pipe else c in
+    field.(index) <- (if index = start_row then String.mapi f line else line);
+    succ index in
+  ignore (List.fold_left init 0 lines);
+  {field; start; loop = CoordSet.empty}
+
+let square = parse_field (String.split_on_char '\n' (String.trim {|
+-L|F7
+7S-7|
+L|7||
+-L-J|
+L|-JF
+|}))
+
+let complex = parse_field (String.split_on_char '\n' (String.trim {|
+7-F7-
+.FJ|7
+SJLL7
+|F--J
+LJ.LJ
+|}))
+
+let get_start {field; start = (row, column) as start} =
+  let first, second =
+    match field.(row).[column] with
+    | '|' -> ((row - 1, column), (row + 1, column))
+    | '-' -> ((row, column - 1), (row, column + 1))
+    | 'J' -> ((row - 1, column), (row, column - 1))
+    | '7' -> ((row + 1, column), (row, column - 1))
+    | 'L' -> ((row - 1, column), (row, column + 1))
+    | 'F' -> ((row + 1, column), (row, column + 1))
+    | _ -> assert false in
+  ((start, first), (start, second))
+
+let step {field} (prev, (row, column)) =
+  let ((delta1_row, delta1_column), (delta2_row, delta2_column)) =
+    CharMap.find field.(row).[column] connections in
+  let next1 = (row + delta1_row, column + delta1_column) in
+  let next2 = (row + delta2_row, column + delta2_column) in
+  if next1 = prev then
+    next2
+  else if next2 = prev then
+    next1
+  else
+    assert false
+
+let rec walk field loop n (((_, cur1) as walk1), ((_, cur2) as walk2)) =
+  let n = succ n in
+  if cur1 = cur2 then
+    n, {field with loop}
+  else
+    let next1 = step field walk1 in
+    let next2 = step field walk2 in
+    if next1 = cur2 && next2 = cur1 then
+      n, {field with loop}
+    else
+      let loop = CoordSet.add next1 (CoordSet.add next2 loop) in
+      walk field loop n ((cur1, next1), (cur2, next2))
+
+let walk field =
+  let ((start, next1), (_, next2)) as start_state = get_start field in
+  let loop = CoordSet.of_list [start; next1; next2] in
+  walk field loop 0 start_state
+
+let test1_part1, square = walk square
+let test2_part1, complex = walk complex
+let field =
+  In_channel.with_open_text "input-10" @@ fun ic ->
+    parse_field (In_channel.input_lines ic)
+let solution_part1, field = walk field
+
 
 let () =
   Printf.printf "Day 10; Puzzle 1; test1 = %d\n\
