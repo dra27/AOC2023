a58bb0942b5c73883598fc175f1710e3a51ffcd2
David Allsopp <david.allsopp@metastack.com>
2024-08-13 18:05:36 +0100

    Day 11 - second puzzle

--- day11.ml
+++ day11.ml
@@ -1,83 +1,57 @@
-type universe = string list
+module String = struct
+  include String
 
-let print_universe fmt (universe : universe) =
-  let print_line line =
-    Format.pp_print_string fmt line;
-    Format.pp_print_newline fmt () in
-  Format.pp_print_newline fmt ();
-  List.iter print_line universe
+  let foldi_left f x a =
+    let r = ref x in
+    for i = 0 to length a - 1 do
+      r := f !r i (unsafe_get a i)
+    done;
+    !r
 
-let test : universe = String.split_on_char '\n' (String.trim {|
-...#......
-.......#..
-#.........
-..........
-......#...
-.#........
-.........#
-..........
-.......#..
-#...#.....
-|})
+  let fold_lefti_map f x a =
+    let r = ref x in
+    let l = length a in
+    let b = Bytes.create l in
+    for i = 0 to l - 1 do
+      let r', c = f !r i (unsafe_get a i) in
+      r := r';
+      Bytes.unsafe_set b i c
+    done;
+    !r, Bytes.unsafe_to_string b
+end
 
-let universe : universe =
-  In_channel.with_open_text "input-11" @@ fun ic ->
-    In_channel.input_lines ic
-
-let[@tail_mod_cons] rec expand_vertically columns = function
-| line::lines ->
-    let scan (b, i) c =
-      if c = '.' then
-        (b, succ i)
-      else begin
-        columns.(i) <- false;
-        (false, succ i)
-      end in
-    if fst (String.fold_left scan (true, 0) line) then
-      line::line::(expand_vertically columns lines)
-    else
-      line::(expand_vertically columns lines)
-| [] -> []
-
-let[@tail_mod_cons] rec compute_expansion columns start i =
-  if i = Array.length columns then
-    if start >= 0 then
-      [(start, i - start)]
-    else
-      [(i, 0)]
-  else
-    if columns.(i) then
-      compute_expansion columns (if start >= 0 then start else i) (succ i)
-    else if start >= 0 then
-      (start, i - start)::(compute_expansion columns (-1) (succ i))
-    else
-      compute_expansion columns start (succ i)
-
-let compute_expansion columns = compute_expansion columns (-1) 0
-
-let expand_horizontally expansion line =
-  let expand_horizontally (last_index, last_length) ((index, length) as next) =
-    let segment =
-      let init i =
-        if i < index then
-          line.[i + last_index]
+let parse lines =
+  let analyse (galaxies, empty_rows, row, mask) line =
+    let galaxies', mask =
+      let f galaxies i c =
+        if c = '#' then
+          ((row, i)::galaxies, c)
         else
-          '.'
-      in
-      String.init (index - last_index + length) init in
-    next, segment in
-  let _, segments = List.fold_left_map expand_horizontally (0, 0) expansion in
-  String.concat "" segments
-
-let expand lines : universe =
+          (galaxies, mask.[i]) in
+      String.fold_lefti_map f galaxies line in
+    let empty_rows =
+      if galaxies == galaxies' then row::empty_rows else empty_rows in
+    (galaxies', empty_rows, succ row, mask) in
   match lines with
-  | line::_ ->
-      let length = String.length line in
-      let columns = Array.make length true in
-      let lines = expand_vertically columns lines in
-      let expansions = compute_expansion columns in
-      List.map (expand_horizontally expansions) lines
+  | hd::_ ->
+      let galaxies, empty_rows, _, mask =
+        let mask = String.make (String.length hd) '.' in
+        List.fold_left analyse ([], [], 0, mask) lines in
+      let empty_columns =
+        let f empty_columns i = function
+        | '.' -> i::empty_columns
+        | _ -> empty_columns in
+        String.foldi_left f [] mask in
+      (galaxies, empty_rows, empty_columns)
   | [] -> assert false
+
+let expand n (galaxies, empty_rows, empty_columns) =
+  let f ref acc this = if ref > this then n + acc else acc in
+  let expand (row, column) =
+    let row = List.fold_left (f row) row empty_rows in
+    let column = List.fold_left (f column) column empty_columns in
+    (row, column) in
+  List.map expand galaxies
 
 let[@tail_mod_cons] rec all_pairs xs x = function
 | y::ys ->
@@ -92,6 +66,11 @@ let all_pairs xs =
   | x::_ -> all_pairs xs x []
   | [] -> []
 
+let shortest_paths galaxies =
+  let f acc ((x1, y1), (x2, y2)) =
+    abs (x2 - x1) + abs (y2 - y1) + acc in
+  List.fold_left f 0 (all_pairs galaxies)
+
 let find_galaxies universe =
   let find_in_row (acc, y) row =
     let find_in_column (acc, x) c =
@@ -103,11 +82,30 @@ let find_galaxies universe =
     acc, succ y in
   fst (List.fold_left find_in_row ([], 0) universe)
 
-let part1 universe =
-  let f acc ((x1, y1), (x2, y2)) =
-    abs (x2 - x1) + abs (y2 - y1) + acc in
-  List.fold_left f 0 (all_pairs (find_galaxies (expand universe)))
+let test = parse (String.split_on_char '\n' (String.trim {|
+...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....
+|}))
+
+let universe =
+  In_channel.with_open_text "input-11" @@ fun ic ->
+    parse (In_channel.input_lines ic)
 
 let () =
   Printf.printf "Day 11; Puzzle 1; test = %d\n\
-                 Day 11; Puzzle 1 = %d\n" (part1 test) (part1 universe)
+                 Day 11; Puzzle 1 = %d\n\
+                 Day 11; Puzzle 2; test1 = %d\n\
+                 Day 11; Puzzle 2; test2 = %d\n\
+                 Day 11; Puzzle 2 = %d\n" (shortest_paths (expand 1 test))
+                                          (shortest_paths (expand 1 universe))
+                                          (shortest_paths (expand 9 test))
+                                          (shortest_paths (expand 99 test))
+                                          (shortest_paths (expand 999999 universe))
